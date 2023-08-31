60e3e60d592bffb8fbbd11460bedb1fc9673e6d5
cliff76 <270462+cliff76@users.noreply.github.com>
2023-08-31 10:04:17 -0700

    Adds initial scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import {Container} from "react-bootstrap";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home.tsx";
+import {Store} from "./pages/Store.tsx";
+import {About} from "./pages/About.tsx";
+
 function App() {
   return (
-      <h1>Hello World!</h1>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/store" element={<Store/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </Container>
   )
 }
 
