df5bc57609c89807a64e5cbbbf6d90bc9d7b24ea
cliff76 <270462+cliff76@users.noreply.github.com>
2023-09-05 09:49:31 -0700

    Implements ShoppingCart Context

--- src/context/ShoppingCartProvider.tsx
+++ src/context/ShoppingCartProvider.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState} from "react";
+import {createContext, ReactNode, useContext, useState} from "react";
 
 type ShoppingCartProviderProps = {
     children: ReactNode;
@@ -17,10 +17,41 @@ type CartItem = {
     quantity: number
 }
 
+export function useShoppingCart() {
+    return useContext(ShoppingCartContext)
+}
 export function ShoppingCartProvider({children}: ShoppingCartProviderProps) {
     const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+    function findCartItem(id: number) {
+        return cartItems.find((cartItem: CartItem) => cartItem.id == id);
+    }
+
+    const getItemQuantity = (id: number) => {
+        const cartItem = findCartItem(id);
+        return cartItem == undefined ? 0 : cartItem.quantity
+    }
+
+    const increaseCartQuantity = (id: number) => {
+        setCartItems(prevCartItems => {
+            return findCartItem(id) == undefined ? [...prevCartItems, {id, quantity:1}] : prevCartItems.map(each => each.id == id ? {id, quantity: each.quantity + 1} : each)
+        })
+    }
+
+    const decreaseCartQuantity = (id: number) => {
+        setCartItems(prevCartItems => {
+            return findCartItem(id)?.quantity == 1 ? prevCartItems.filter((item: CartItem) => item.id == id) : prevCartItems.map(each => each.id == id ? {id, quantity: each.quantity - 1} : each)
+        })
+    }
+
+    const removeFromCart = (id: number) => {
+        setCartItems(prevCartItems => {
+            return prevCartItems.filter((item: CartItem) => item.id == id)
+        })
+    }
+
     return (
-        <ShoppingCartContext.Provider value={{}}>
+        <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
             {children}
         </ShoppingCartContext.Provider>
     )
